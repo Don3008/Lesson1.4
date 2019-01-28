94aba84b21fe1140ff1ebab67d980cdd2e867ff9
Don3008 <don3008@yandex.ru>
2019-01-28 15:21:13 +0300

    Добавьте файлы проекта.

---
+++ LeapYear/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double year;
+            Console.WriteLine("Введите год, чтобы узнать, является ли он високосным:");
+            year = Convert.ToDouble(Console.ReadLine());
+            if (year > 0 && year % 1 == 0)
+            {
+                if (year % 400 == 0)
+                {
+                    Console.WriteLine("Год " + year + " - високосный");
+                }
+                else if (year % 100 == 0)
+                {
+                    Console.WriteLine("Год " + year + " - невисокосный");
+                }
+                else if (year % 4 == 0)
+                {
+                    Console.WriteLine("Год " + year + " - високосный");
+                }
+                else
+                {
+                    Console.WriteLine("Год " + year + " - невисокосный");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введено неверное значения для года");
+            }
+        }
+    }
+}
